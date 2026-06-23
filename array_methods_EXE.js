0c25be56db7a826e23bfa16504a2966bf2d2a5a9
elyasaf waisman <elyasafw1997@gmail.com>
2026-06-23 11:56:53 +0300

    add exe 2, 4

---
+++ array_methods_EXE.js
@@ -0,0 +1,16 @@
+// 1
+// const numsArr = [1, 2, 3, 4, 5]
+// const double = numsArr.map(num => num * 2)
+// console.log(double)
+
+
+// 2
+const textsArr = ["hello", "world"]
+const upperTexts = textsArr.map(word => word.toUpperCase())
+console.log(upperTexts)
+
+
+// 4
+const numsArr = [1, 2, 3, 4, 5, 6]
+const evensNums = numsArr.filter(num => num % 2 === 0)
+console.log(evensNums)
